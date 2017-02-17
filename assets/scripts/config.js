ce3c88a143abbffd870b2e809434ace021c9d7b6
“Sinyru” <znchen720@gmail.comgit config --global pull.rebase truegit config --global branch.autosetuprebase alwaysgit config --global push.default simplegit config --global branch.autosetupmerge truegit config --global core.editor atom>
2017-02-17 15:26:58 -0500

    Changed config file for production to heroku link Created variable board

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 const config = {
   apiOrigins: {
-    production: 'https://ga-wdi-boston.herokuapp.com',
+    production: 'https://git.heroku.com/connect-five-api.git'
   },
 };
 
---
+++ assets/scripts/gameEngine.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let board = [];
+for(let i=0;i<49;i++){
+  board.push('');
+}
+
+module.exports = {
+  board
+};

cf7968c6052fc473df42a728c4dd9b233f9bf2f6
“Sinyru” <znchen720@gmail.comgit config --global pull.rebase truegit config --global branch.autosetuprebase alwaysgit config --global push.default simplegit config --global branch.autosetupmerge truegit config --global core.editor atom>
2017-02-17 15:40:54 -0500

    Created methods and variables
    Board = variable to hold the grid
    ChangingTurns = to switch turns between players
    SelectSpace = to select space within the grid.

--- assets/scripts/gameEngine.js
+++ assets/scripts/gameEngine.js
@@ -1,10 +1,26 @@
 'use strict';
 
 let board = [];
-for(let i=0;i<49;i++){
+for (let i = 0; i < 49; i++) {
   board.push('');
 }
 
+let currentPlayer = 'o';
+const changingTurns = function() {
+  if (currentPlayer === 'x') {
+    currentPlayer = 'o';
+  } else if (currentPlayer === 'o') {
+    currentPlayer = 'x';
+  }
+  return currentPlayer;
+};
+
+const selectSpace = function(i) {
+  board[i] = changingTurns();
+};
+
 module.exports = {
-  board
+  board,
+  changingTurns,
+  selectSpace,
 };
